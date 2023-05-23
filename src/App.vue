026699fa4f441c009764bc0a8cf930c8a0e5b49b
yaTaoh <1358103256@qq.com>
2023-05-23 23:50:16 +0800

    demo02:第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="ox" v-show="key"></div>
+    <button @click="key =! key">{{ key?'隐藏':'显示' }}</button>
+    <br><hr>
+    <select name="" id="" v-model="num">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+
+    <!-- 案例3 -->
+    <h2>{{title}}</h2>
+    <h3>{{channel}}</h3>
+    
+    <button @click="edit" >编辑</button>
+    <br>
+    <div class="fore" v-show="key1">
+      <div>
+        标题：<input type="text" placeholder="请输入标题" v-model="title1">
+      </div>
+      <div>
+        频道：<input type="text" placeholder="请输入频道" v-model="channel1">
+      </div>
+      <button @click="cannel">取消</button>
+      <button @click="ok">确认</button>
+    </div>
+    
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      key :true,
+      num:2,
+      title:"标题1",
+      channel:"频道1",
+      title1:"",
+      channel1:"",
+      key1:false
+    }
+  },
+  methods:{
+    edit(){
+      this.title1 = this.title
+      this.channel1 = this.channel
+      this.key1 =! this.key1
+
+    },
+    ok(){
+      this.title = this.title1
+      this.channel = this.channel1
+      this.title1 = ""
+      this.channel1 = ""
+      this.key1 =! this.key1
+    },
+    cannel(){
+      this.title1 = ""
+      this.channel1 = ""
+      this.key1 =! this.key1
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .ox {
+    width: 50px;
+    height: 50px;
+    background-color: #888;
+    
+  };
+  /* .fore {
+    display: none;
+
+  }; */
+  
 </style>
---
+++ src/day1.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <p>123456</p>
+    <a :href="msg">{{ name[1] }}</a>
+    <p>
+      {{ num }}
+      <button @click="add(2)">加几个件</button>
+    </p>
+    <a href="msg1" @click.prevent="fn1">必应</a>
+    <br>
+
+    <!-- 翻转案例 -->
+    <!-- 点击需要注册事件 -->
+    <!-- 得到数值参数，转为数组，反转，输出，接收 -->
+    <h2>翻转案例</h2>
+    <h3>{{ anli }}</h3>
+    <button @click="reve">反转</button>
+    <br>
+
+    <!-- 案例2：按钮控制隐藏 -->
+    <!-- 点击说明需要注册事件 -->
+    <!-- 按钮控制先隐藏 -->
+    <div class="anli2" v-show="key"></div>
+    <button class="bt2" @click="key =! key">{{ key?'显示':'隐藏' }}</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      num:1,
+      info:'zhangsan',
+      name:["123","456","789"],
+      obj:{
+        name:"moxing",
+        age:19
+      },
+      msg:"https://www.baidu.com/",
+      msg1:"https://cn.bing.com/?pc=BG00",
+      anli:"这是一对数字和一堆文字",
+      key:true
+    }
+  },
+  methods:{
+    add(a){
+      this.num+=a
+    },
+    fn1(){
+      console.log('123');
+    },
+    reve(){
+      let old = this.anli.split("")
+      let old1 = old.reverse()
+      let anliNew = old1.join("")
+      this.anli = anliNew
+    }
+    
+  }
+
+}
+</script>
+
+<style lang="less">
+  span{
+    background-color: pink;
+  };
+  .anli2{
+    width: 50px;
+    height: 50px;
+    background-color: #222;
+  }
+  
+
+</style>
